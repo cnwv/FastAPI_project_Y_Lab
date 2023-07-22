6b09b5af54eac4f1d2ee4e76469158620d15a423
cnwv <115669384+cnwv@users.noreply.github.com>
2023-07-23 00:48:04 +0400

    add submenu's api

---
+++ src/api/CRUD/submenus.py
@@ -0,0 +1,81 @@
+from sqlalchemy import select, insert, update, delete, func
+from .schemas import Submenu, Dish
+from pydantic import BaseModel
+
+
+class SubmenuModel(BaseModel):
+    id: str
+    title: str
+    description: str
+
+    class Config:
+        orm_mode = True
+
+
+class CRUDSubmenu():
+    @staticmethod
+    def prepare_response(submenu, menu_id):
+        response = dict()
+        response['id'] = submenu.id
+        response['title'] = str(submenu.title)
+        response['description'] = str(submenu.description)
+        response['menu_id'] = int(menu_id)
+        return response
+
+    @staticmethod
+    def get_submenu_and_dish_count(submenu_id, session):
+        dish_count = session.query(func.count(Dish.id)).join(Submenu).filter(Submenu.id == submenu_id).scalar()
+        return dish_count
+
+    @staticmethod
+    def get_submenus(menu_id, session):
+        result = {'Submenus': []}
+        submenus = session.query(Submenu).where(Submenu.menu_id == menu_id).all()
+        if submenus:
+            for submenu in submenus:
+                dish_count = CRUDSubmenu.get_submenu_and_dish_count(submenu.id, session)
+                result['Submenus'].append(
+                    {'Submenu ID': submenu.id, 'Title': submenu.title, 'Dish Count': dish_count}
+                )
+            return result
+        return []
+
+    @staticmethod
+    def create_submenu(target_menu_id, submenu, session):
+        stmt = insert(Submenu).values(menu_id=target_menu_id,
+                                      title=submenu.title,
+                                      description=submenu.description).returning(Submenu)
+        result = CRUDSubmenu.prepare_response(session.execute(stmt).scalar())
+        session.commit()
+        return result
+
+    @staticmethod
+    def get_target_submenu(menu_id, submenu_id, session):
+        query = select(Submenu).where(Submenu.id == submenu_id, Submenu.menu_id == menu_id)
+        result = session.execute(query).scalar()
+        if result:
+            result = CRUDSubmenu.prepare_response(result)
+            return result
+        else:
+            return []
+
+    @staticmethod
+    def update_target_submenu(menu_id, submenu_id, submenu, session):
+        stmt = session.execute(update(Submenu).where(Submenu.id == submenu_id,
+                                                     Submenu.menu_id == menu_id).values(**submenu.dict()).returning(
+            Submenu))
+        updated_row = stmt.scalar()
+        if updated_row:
+            session.commit()
+            result = CRUDSubmenu.prepare_response(updated_row, menu_id)
+            return result
+        return []
+
+    @staticmethod
+    def delete_target_submenu(menu_id, submenu_id, session):
+        stmt = session.execute(delete(Submenu).where(Submenu.id == submenu_id,
+                                                     Submenu.menu_id == menu_id))
+        if stmt.rowcount > 0:
+            session.commit()
+            return True
+        return False
--- src/api/endpoints/submenus.py
+++ src/api/endpoints/submenus.py
@@ -1,4 +1,10 @@
 from fastapi import APIRouter
+from src.api.CRUD.submenus import CRUDSubmenu
+from config import get_sync_session
+from sqlalchemy.orm import Session
+from fastapi import Depends, Response, status
+from pydantic import BaseModel
+from fastapi import HTTPException
 
 router = APIRouter(
     prefix='/api/v1/menus/{menu_id}/submenus',
@@ -6,26 +12,58 @@ router = APIRouter(
 )
 
 
+class SubmenuBody(BaseModel):
+    title: str
+    description: str
+
+
 @router.get("/")
-def get_submenus():
-    return
+def get_submenus(menu_id: int,
+                 session: Session = Depends(get_sync_session)):
+    result = CRUDSubmenu.get_submenus(menu_id, session)
+    if result:
+        return result
+    return []
 
 
 @router.post("/")
-def create_submenu(submenu_id):
-    return
+def create_submenu(response: Response,
+                   menu_id: int, submenu: SubmenuBody,
+                   session: Session = Depends(get_sync_session)):
+    result = CRUDSubmenu.create_submenu(menu_id, submenu, session)
+    response.status_code = status.HTTP_201_CREATED
+    return result
 
 
 @router.get("/{submenu_id}")
-def get_target_submenu(submenu_id):
-    return
+def get_target_submenu(menu_id: int,
+                       submenu_id: int,
+                       session: Session = Depends(get_sync_session)):
+    result = CRUDSubmenu.get_target_submenu(menu_id, submenu_id, session)
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Submenu not found.")
 
 
 @router.patch("/{submenu_id}")
-def update_target_menu(submenu_id):
-    return
+def update_target_menu(menu_id: int,
+                       submenu_id: int,
+                       submenu: SubmenuBody,
+                       session: Session = Depends(get_sync_session)):
+    result = CRUDSubmenu.update_target_submenu(menu_id, submenu_id, submenu, session)
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Submenu not found.")
 
 
 @router.delete("/{submenu_id}")
-def delete_target_submenu(menu_id):
-    return
+def delete_target_submenu(menu_id: int,
+                          submenu_id: int,
+                          session: Session = Depends(get_sync_session)):
+    result = CRUDSubmenu.delete_target_submenu(menu_id, submenu_id, session)
+    if result:
+        return f'Menu ID {submenu_id} deleted successfully.'
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Submenu not found.")
